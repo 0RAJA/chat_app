02cf4931e4868b1fd4901e09bc596ebc17399e55
raja <1647193241@qq.com>
2022-08-10 16:53:33 +0800

    调整chat结构,增加注释

--- src/model/chat/common.go
+++ src/model/chat/common.go
@@ -1,5 +1,7 @@
 package chat
 
+// chat 中事件的关键字,以Server开头的事件为服务端发送的事件，以Client开头的事件为客户端发送的事件
+
 const (
 	ServerSendMsg      = "send_msg"
 	ServerReadMsg      = "read_msg"
--- src/model/chat/server/handle.go
+++ src/model/chat/server/handle.go
@@ -1,5 +1,7 @@
 package server
 
+// chat 中 server 端有关处理的结构
+
 type AccountLogin struct {
 	EnToken string `json:"en_token"` // 加密后的Token
 	Address string `json:"address"`  // 地址
