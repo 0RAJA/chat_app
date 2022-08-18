a09caaa659526f0588df566267ad08cc9b625b38
raja <1647193241@qq.com>
2022-08-18 10:47:30 +0800

    docker-compose 配置文件修改

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -e #确保脚本在指令返回非零直接返回
 
 echo "run db migrate"
 
+echo "chat_postgres_zr"
 ./migrate -path /app/migration -database "postgresql://root:123456@chat_postgres_zr:5432/chat?sslmode=disable" -verbose up
 
 echo "start the app"
