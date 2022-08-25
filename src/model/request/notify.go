a72577dcb6803d4d4b97071cd7d9f8c9d36c8375
sherlock-chong <2682072219@qq.com>
2022-08-25 11:23:33 +0800

    add notify delete

--- src/model/request/notify.go
+++ src/model/request/notify.go
@@ -8,11 +8,15 @@ type CreateNotify struct {
 	MsgExtend  *model.MsgExtend `json:"msg_expand"  form:"msg_expand"`
 }
 type UpdateNotify struct {
-	ID         int64            `json:"id" form:"id" binding:"required" binding:"required"`
+	ID         int64            `json:"id" form:"id" binding:"required"`
 	RelationID int64            `json:"relation_id" form:"relation_id" binding:"required"`
 	MsgContent string           `json:"msg_content" form:"msg_content" binding:"required"`
 	MsgExtend  *model.MsgExtend ` json:"msg_expand"  form:"msg_expand"`
 }
 type GetNotifyByID struct {
-	RelationID int64 `json:"relation_id" form:"relation_id"`
+	RelationID int64 `json:"relation_id" form:"relation_id" binding:"required"`
+}
+type DeleteNotify struct {
+	ID         int64 `json:"id" form:"id" binding:"required"`
+	RelationID int64 `json:"relation_id" form:"relation_id" binding:"required"`
 }
